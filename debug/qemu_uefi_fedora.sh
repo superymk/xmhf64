50934ea8987dccd2f721b815d1d32d3fc99f69a8
Miao Yu <superymk@cmu.edu>
2024-04-01 17:54:42 -0400

    SecBase: qemu debug supports tpm

--- debug/qemu_uefi_fedora.sh
+++ debug/qemu_uefi_fedora.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run the TPM emulator in the background.
+./qemu_tpm_emulator.sh &
+
 # Run QEMU (download debian11efi.qcow2 from Google Drive, put it to /PATH/TO/)
 # /usr/share/OVMF/OVMF_CODE.fd is UEFI firmware image
 # /usr/share/OVMF/UefiShell.iso contains UEFI shell
@@ -10,10 +13,12 @@ qemu-system-x86_64 \
 	-gdb tcp::1234 \
 	-smp 4 \
 	-cpu Haswell,vmx=yes \
+	-machine q35 -device intel-iommu \
 	-enable-kvm \
 	-bios /usr/share/OVMF/OVMF_CODE.fd \
 	-net none \
 	-serial stdio \
+	-chardev socket,id=chrtpm,path=/tmp/emulated_tpm/swtpm-sock \
 	-drive media=cdrom,file=/usr/share/OVMF/UefiShell.iso,index=0 \
 	-drive media=cdrom,file=../uefi_flushdrive_img/grub/fat.img,index=1 \
 	-drive media=disk,file=debian11efi.qcow2,index=2
--- debug/qemu_uefi_ubuntu.sh
+++ debug/qemu_uefi_ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Run the TPM emulator in the background.
+./qemu_tpm_emulator.sh &
+
 # Run QEMU (download debian11efi.qcow2 from Google Drive, put it to /PATH/TO/)
 # /usr/share/OVMF/OVMF_CODE.fd is UEFI firmware image
 # /usr/share/OVMF/UefiShell.iso contains UEFI shell
@@ -11,10 +14,13 @@ qemu-system-x86_64 \
 	-gdb tcp::1234 \
 	-smp 4 \
 	-cpu Haswell,vmx=yes \
+	-machine q35 -device intel-iommu \
 	-enable-kvm \
 	-bios /usr/share/ovmf/OVMF.fd \
 	-net none \
 	-serial stdio \
+	-chardev socket,id=chrtpm,path=/tmp/emulated_tpm/swtpm-sock \
+	-tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0 \
 	-drive media=cdrom,file=../uefi_flushdrive_img/grub/fat.img,index=0 \
 	-drive media=cdrom,file=../uefi_flushdrive_img/grub/fat.img,index=1 \
 	-drive media=disk,file=debian11efi.qcow2,index=2
